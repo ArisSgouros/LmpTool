1d8d204beb7f79203b8f36db6d77e72a2111a966
ArisSgouros <arissgouros@gmail.com>
2023-10-17 14:12:09 +0300

    test(DatafileMerge):upd example

--- DatafileMerge/example_brush_wall/run.sh
+++ DatafileMerge/example_brush_wall/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python ../datafile_merge.py pos_brush.dat pos_wall.dat pos_brush_wall.dat -z 6 > o.merge.log
+python ../datafile_merge.py pos_brush.dat pos_wall.dat pos_brush_wall.dat -side -z -poffset 6 > o.merge.log
