8bbcc9297f7b4d91552a2db51f8e4e2ce67b7570
ArisSgouros <arissgouros@gmail.com>
2023-11-08 12:17:32 +0200

    test(DataToDump): add test for xyz files

--- DataToDump/example/run.sh
+++ DataToDump/example/run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-python ../data_to_dump.py in.pos.dat full --fmt="id,type,mol,x,y,z" -dump_file="o.dump.lammpstrj"
-python ../data_to_dump.py in.pos_im.dat full --fmt="id,type,mol,x,y,z" -dump_file="o.dump_im.lammpstrj"
+python ../data_to_dump.py in.pos.dat full -file_type="xyz" --fmt="type,x,y,z" -dump_file="o.dump.xyz"
+python ../data_to_dump.py in.pos.dat full -file_type="lammpstrj" --fmt="id,type,mol,x,y,z" -dump_file="o.dump.lammpstrj"
+python ../data_to_dump.py in.pos_im.dat full -file_type="lammpstrj" --fmt="id,type,mol,x,y,z" -dump_file="o.dump_im.lammpstrj"
