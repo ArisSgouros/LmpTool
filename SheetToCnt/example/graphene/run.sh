3f13f32e3e9377ba791c3a7cc4a06a70cf06f650
ArisSgouros <arissgouros@gmail.com>
2024-07-31 14:36:09 +0300

    test(SheetToCnt): add tests

--- SheetToCnt/example/graphene/run.sh
+++ SheetToCnt/example/graphene/run.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
-python ../../sheet_to_cnt.py in.sq10x20x1.dat o.sq10x20x1_xz.dat x z
-#python ../../sheet_to_cnt.py in.sq10x20x1.dat o.sq10x20x1_yz.dat y z
-#python ../../sheet_to_cnt.py o.sq10x20x1_xz.dat o.sq10x20x1_xz_xz.dat z x
+python ../../sheet_to_cnt.py in.sq10x20x1.dat o.sq10x20x1_xz.dat x z > o.sq10x20x1_xz.log
 
-#python ../../sheet_to_cnt.py in.sq1x10x20.dat o.sq1x10x20_yx.dat y x
-#python ../../sheet_to_cnt.py in.sq1x10x20.dat o.sq1x10x20_zx.dat z x
+# displace along reference
+python ../../sheet_to_cnt.py in.sq10x20x1_zref15.dat o.sq10x20x1_zref15_xz.dat x z > o.sq10x20x1_zref15_xz.log
 
-#python ../../sheet_to_cnt.py in.20_10.dat o.20_10.dat x z
+
+python ../../sheet_to_cnt.py in.sq10x20x1.dat o.sq10x20x1_yz.dat y z > o.sq10x20x1_yz.log
+
+
+#
+python ../../sheet_to_cnt.py o.sq10x20x1_xz.dat o.sq10x20x1_xz_yx.dat y x > o.sq10x20x1_xz_yx.log
+
+python ../../sheet_to_cnt.py in.sq1x10x20.dat o.sq1x10x20_yx.dat y x > o.sq1x10x20_yx.log
+python ../../sheet_to_cnt.py in.sq1x10x20.dat o.sq1x10x20_zx.dat z x > o.sq1x10x20_zx.log
+
+python ../../sheet_to_cnt.py in.200_4.dat o.200_4_xz.dat y z > o.200_4_xz.log
+python ../../sheet_to_cnt.py o.200_4_xz.dat o.200_4_xz_yx.dat x y > o.200_4_xz_yx.log
+
