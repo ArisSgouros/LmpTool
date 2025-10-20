7959120a17fc3f3b2657bc26935f0f6ced96688b
ArisSgouros <arissgouros@gmail.com>
2025-10-20 14:48:04 +0300

    build(ExtXyz): rnm example

--- ExtXyz/example/extxyz_io/this/test_this_io.py
+++ ExtXyz/example/extxyz_io/this/test_this_io.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 # Add the parent directory to sys.path
-parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../'))
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../'))
 sys.path.insert(0, parent_dir)
 
 from extxyz import read_xyz_file, write_xyz_file
