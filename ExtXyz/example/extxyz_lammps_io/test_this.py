b10b3e51f3b0a3e47267b27d1733b842e7040145
ArisSgouros <arissgouros@gmail.com>
2025-10-20 15:07:16 +0300

    test(ExtXyz): add extxyz_lammps_io

---
+++ ExtXyz/example/extxyz_lammps_io/test_this.py
@@ -0,0 +1,14 @@
+import os
+import sys
+# Add the parent directory to sys.path
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../src/'))
+sys.path.insert(0, parent_dir)
+
+from ase_lammps_io import read_lammps, write_xyz, read_xyz, write_lammps
+
+atoms = read_lammps("in.data", style="atomic")
+write_xyz(atoms, "o.xyz")
+
+atoms2 = read_xyz("o.xyz", frame=0)
+write_lammps(atoms2, "o.data", style="atomic", units="metal")
+
