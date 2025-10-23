7543992b60a765799356df048694ca780ec17bb5
ArisSgouros <arissgouros@gmail.com>
2025-10-23 19:08:39 +0300

    test(ParseQeOutput/example_dev): add json

--- ParseQeOutput/example_dev/auto.sh
+++ ParseQeOutput/example_dev/auto.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-python test.py
+
+path_pwo="../../QuantumEspressoHelper/parse_pwo.py"
+
+# parse single output files
+python $path_pwo i.MoSSe_3x3_01_vcrelax.out --pretty -o o.01.json
+python $path_pwo i.MoSSe_3x3_02_relax.out --pretty -o o.02.json
+python $path_pwo i.MoSSe_3x3_03_scf.out --pretty -o o.03.json
+python $path_pwo i.MoSSe_3x3_04_scf_no_force_no_stress.out --pretty -o o.04.json
