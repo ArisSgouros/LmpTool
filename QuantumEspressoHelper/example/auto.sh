09536162cdbf18fd9de93b86eaf2f0abb7d0ba7e
ArisSgouros <arissgouros@gmail.com>
2025-10-24 17:50:52 +0300

    squash w/ prev

--- QuantumEspressoHelper/example/auto.sh
+++ QuantumEspressoHelper/example/auto.sh
@@ -7,9 +7,12 @@ json2xyz="../json2xyz.py"
 for case_ in 01a_MoSSe.scf_lat-ang.in 01b_MoSSe.scf_ang-lat.in 02a_MoSSe.relax.in; do
     python $pwi2json $case_ --pretty -o $case_.json
 done
+python ../pwi2json.py "./01*.in" --pretty > 01ba_in.combined.json
 
 for case_ in 01c_MoSSe.scf.out 02b_MoSSe.relax.out 03b_MoSSe.vcrelax_lat.out 03c_MoSSe.vcrelax_ang.out; do
     python $pwo2json $case_ --pretty -o $case_.json
 done
+python ../pwo2json.py "./03*.out" --pretty > 03cb_out.combined.json
 
-python ../pwo2json.py "./03*.out" --pretty > 03bc.out.combined.json
+
+#python ../json2xyz.py 03bc.out.combined.json
