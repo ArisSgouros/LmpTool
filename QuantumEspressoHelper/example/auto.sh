f32edefc520985a18d9f49ca6bea58304f00f91c
ArisSgouros <arissgouros@gmail.com>
2025-10-24 17:35:17 +0300

    test(QuantumEspressoHelper): rework example

--- QuantumEspressoHelper/example/auto.sh
+++ QuantumEspressoHelper/example/auto.sh
@@ -4,24 +4,10 @@ pwi2json="../pwi2json.py"
 pwo2json="../pwo2json.py"
 json2xyz="../json2xyz.py"
 
-# parse single input files
-python $pwi2json MoSSe_3x3_scf.in --pretty -o MoSSe_3x3_scf.in.json
+for case_ in 01a_MoSSe.scf_lat-ang.in 01b_MoSSe.scf_ang-lat.in 02a_MoSSe.relax.in; do
+    python $pwi2json $case_ --pretty -o $case_.json
+done
 
-# parse several input files
-python $pwi2json "./*.in" --pretty > MoSSe_3x3_scf.in.many.json
-
-# parse single output files
-python $pwo2json MoSSe_3x3_scf.out --pretty -o MoSSe_3x3_scf.out.json
-python $pwo2json  MoSSe_3x3_scf_no_force_stress.out --pretty -o MoSSe_3x3_scf_no_force_stress.out.json
-python $pwo2json  MoSSe_3x3_relax.out --pretty -o MoSSe_3x3_relax.out.json
-python $pwo2json  MoSSe_3x3_vcrelax.out --pretty -o MoSSe_3x3_vcrelax.out.json
-
-# parse sevaral output files
-python $pwo2json  "./MoSSe_3x3_relax*.out" --pretty > MoSSe_3x3_scf.out.many.json
-
-# convert to extxyz
-python $json2xyz MoSSe_3x3_scf.in.json  > MoSSe_3x3_scf.in.xyz
-python $json2xyz MoSSe_3x3_scf.out.json > MoSSe_3x3_scf.out.xyz
-python $json2xyz MoSSe_3x3_scf.out.json --no-cell --no-forces --no-info  > MoSSe_3x3_scf_lite.out.xyz
-python $json2xyz MoSSe_3x3_scf.out.json --info-keys energy virial > MoSSe_3x3_scf_custom-info.out.xyz
-python $json2xyz MoSSe_3x3_scf_no_force_stress.out.json > MoSSe_3x3_scf_no_force_stress.out.xyz
+for case_ in 01c_MoSSe.scf.out 02b_MoSSe.relax.out 03b_MoSSe.vcrelax_lat.out 03c_MoSSe.vcrelax_ang.out; do
+    python $pwo2json $case_ --pretty -o $case_.json
+done
